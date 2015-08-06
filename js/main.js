87f6e492c403b1877e05e9fcefa9da910999924c
Morpheox <emayoralc@gmail.com>
2015-08-07 00:31:37 +0200

    morale fix 4
    ...

--- js/main.js
+++ js/main.js
@@ -1211,7 +1211,7 @@ for(key in unlocked){
 		case 4: $(".tech_currency").show();unlocked[".tech_currency"]=1;$(".tech_coin").show();unlocked[".tech_coin"]=1;break;
 	}
 	
-	if(maximums["moral"]!=0 or maximums["moral"]!=null)
+	if(maximums["moral"]!=0 || maximums["moral"]!=null)
 	{
 	maximums["morale"]=
     maximums["moral"]=0

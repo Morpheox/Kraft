ddc494202697156589543f1c7126221ad79cf7e4
Morpheox <emayoralc@gmail.com>
2015-08-09 03:16:14 +0200

    intToStringRound

--- js/functions.js
+++ js/functions.js
@@ -9,3 +9,15 @@ function intToString (value) {
     }
     else {return parseFloat(value).toFixed(2);}
     }
+
+function intToStringRound(value) {
+        if (value>10000)
+        {
+    var suffixes = ["", "K", "M", "B","T","C","Q","S"];
+    var suffixNum = Math.floor((""+value).length/3);
+    var shortValue = parseFloat((suffixNum != 0 ? (value / Math.pow(1000,suffixNum)) : value).toPrecision(3));
+    if (shortValue % 1 != 0)  shortNum = shortValue.toFixed(1);
+    return shortValue+suffixes[suffixNum];
+    }
+    else {return Math.round(value);}
+    }

fe4ba347a8ef836aca4246044c67ddd0faefcb06
Morpheox <emayoralc@gmail.com>
2015-08-11 10:56:53 +0200

    hotfix

--- js/main.js
+++ js/main.js
@@ -2686,8 +2686,10 @@ for(key in unlocked){
 		case 5: $(".tech_bronze").show();unlocked[".tech_bronze"]=1;$(".tech_bronzetools").show();unlocked[".tech_bronzetools"]=1;$(".tech_charcoal").show();unlocked[".tech_charcoal"]=1;$(".tech_centralisation").show();unlocked[".tech_centralisation"]=1;break;
 		case 6: $(".tech_steel").show();unlocked[".tech_steel"]=1;$(".tech_manufacturing").show();unlocked[".tech_manufacturing"]=1;$(".tech_steeltools").show();unlocked[".tech_steeltools"]=1;$(".tech_husbandry").show();unlocked[".tech_husbandry"]=1;$(".tech_cavalry").show();unlocked[".tech_cavalry"]=1;break;
 	}
-	if(buildings["library"]>=5){
-		$(".tech_centralisation").show();unlocked[".tech_centralisation"]=1;
+	if(buildings["library"]>=6){
+		$(".tech_steeltools").show();unlocked[".tech_steeltools"]=1;
+		$(".tech_husbandry").show();unlocked[".tech_husbandry"]=1;
+		$(".tech_cavalry").show();unlocked[".tech_cavalry"]=1;
 	}
 
 	

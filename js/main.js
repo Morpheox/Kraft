1309ef768568be3dae0d26f6e7f804ed55a1d02c
Morpheox <emayoralc@gmail.com>
2015-08-11 04:39:32 +0200

    armament fix

--- js/main.js
+++ js/main.js
@@ -1125,7 +1125,7 @@ if (items["wood"]>=woodcost && items["mineral"]>=mineralcost){
 		case 4: $(".tech_currency").show();unlocked[".tech_currency"]=1;$(".tech_exchange").show();unlocked[".tech_exchange"]=1;$(".tech_coin").show();unlocked[".tech_coin"]=1;break;
 		case 5: $(".tech_bronze").show();unlocked[".tech_bronze"]=1;$(".tech_bronzetools").show();unlocked[".tech_bronzetools"]=1;$(".tech_charcoal").show();unlocked[".tech_charcoal"]=1;$(".tech_centralisation").show();unlocked[".tech_centralisation"]=1;break;
 		case 6: $(".tech_steel").show();unlocked[".tech_steel"]=1;$(".tech_manufacturing").show();unlocked[".tech_manufacturing"]=1;$(".tech_steeltools").show();unlocked[".tech_steeltools"]=1;$(".tech_husbandry").show();unlocked[".tech_husbandry"]=1;$(".tech_cavalry").show();unlocked[".tech_cavalry"]=1;break;
-		case 7: $(".tech_leadership").show();unlocked[".tech_leadership"]=1;break;
+		case 7: $(".tech_leadership").show();unlocked[".tech_leadership"]=1;$(".tech_armament").show();unlocked[".tech_armament"]=1;break;
 
 	}
 	
@@ -2542,6 +2542,7 @@ for(key in unlocked){
 
 	if(buildings["library"]>=7){
 	 $(".tech_leadership").show();unlocked[".tech_leadership"]=1;
+	 $(".tech_armament").show();unlocked[".tech_armament"]=1;
 	}
 
 	//RETROCOMPATIBILITY

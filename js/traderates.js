5edfb637beec946c24ee7f5ae58e33c1d3f00d1b
Morpheox <emayoralc@gmail.com>
2015-08-13 06:07:37 +0200

    fix

--- js/traderates.js
+++ js/traderates.js
@@ -175,7 +175,7 @@ clearInterval(tickinterval);
 $(".docklog").html("The fleet its back!<br> You recieve "+intToString(trademission["get"])+" "+trademission["resourceget"]);
 $(".tradego").show()
 if(Math.random()>0.20){
-$(".docklog").html("<br> You also get 1 lock.";
+$(".docklog").html("<br> You also get 1 lock.");
 craft["lock"]++
 
 }

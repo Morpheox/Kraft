25e0056566919cec6ee48f3c33c158535832ac8a
Morpheox <emayoralc@gmail.com>
2015-08-15 09:37:52 +0200

    fix

--- js/technology.js
+++ js/technology.js
@@ -1,18 +1,18 @@
 function researchunlock(){
 
 
-	if(bonus["science"]>=100){
+	if(bonus["science"]>=100 && unlocked[".tech_geology"]==0){
 		$(".tech_geology").show()
 		unlocked[".tech_geology"]=1;
 	}
 
 
-	if(bonus["military"]>=100){
+	if(bonus["military"]>=100 && unlocked[".tech_tactics"]==0){
 		$(".tech_tactics").show()
 		unlocked[".tech_tactics"]=1;
 	}
 
-	if(bonus["economy"]>=100){
+	if(bonus["economy"]>=100 && unlocked[".tech_funding"]==0){
 		$(".tech_funding").show()
 		unlocked[".tech_funding"]=1;
 	}

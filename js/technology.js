0da4aeb6f60372a49e64aada26a008cbe24e39da
Morpheox <emayoralc@gmail.com>
2015-08-15 12:08:16 +0200

    fix

--- js/technology.js
+++ js/technology.js
@@ -1,23 +1,23 @@
 function researchunlock(){
 
 
-	if(bonus["science"]>=100 && unlocked[".tech_geology"]==0){
+	if(bonus["science"]>=100 && unlocked[".tech_geology"]!=1){
 		$(".tech_geology").show()
 		unlocked[".tech_geology"]=1;
 	}
 
 
-	if(bonus["military"]>=100 && unlocked[".tech_tactics"]==0){
+	if(bonus["military"]>=100 && unlocked[".tech_tactics"]!=1){
 		$(".tech_tactics").show()
 		unlocked[".tech_tactics"]=1;
 	}
 
-	if(bonus["economy"]>=100 && unlocked[".tech_funding"]==0){
+	if(bonus["economy"]>=100 && unlocked[".tech_funding"]!=1){
 		$(".tech_funding").show()
 		unlocked[".tech_funding"]=1;
 	}
 
-	if(bonus["military"]>=500 && unlocked[".tech_healing"]==0){
+	if(bonus["military"]>=500 && unlocked[".tech_healing"]!=1){
 		$(".tech_healing").show()
 		unlocked[".tech_healing"]=1;
 	}

1952ec1ae66e92e808503aa28d0b6030b7faa332
Morpheox <emayoralc@gmail.com>
2015-08-21 23:30:17 +0200

    fix

--- js/functions.js
+++ js/functions.js
@@ -38,3 +38,24 @@ function totime(b){
 function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+function clearListCookies()
+{   
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++)
+    {   
+        var spcook =  cookies[i].split("=");
+        deleteCookie(spcook[0]);
+    }
+    function deleteCookie(cookiename)
+    {
+        var d = new Date();
+        d.setDate(d.getDate() - 1);
+        var expires = ";expires="+d;
+        var name=cookiename;
+        //alert(name);
+        var value="";
+        document.cookie = name + "=" + value + expires + "; path=/acc/html";                    
+    }
+
+}
